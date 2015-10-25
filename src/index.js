b8eb82b00d6782448cc199d508b07a0a827abfdf
suǝʞǝǝpʇ <tobias.deekens@epost-dev.de>
2015-10-25 14:18:46 +0100

    chore(index): add debugging code to fetch topics and specific topic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+import { Topics, Topic } from 'http';
+
 export default function() {
   return true;
 }
+
+function run() {
+  let topics = new Topics();
+  let topic = new Topic();
+
+  topics.get().then(topics => {
+    console.log(topics);
+  });
+
+  topic.get('1751295897__Berlin').then(topic => {
+    console.log(topic);
+  });
+}
+
+run()
