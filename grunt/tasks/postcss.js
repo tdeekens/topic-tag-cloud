679d15835ba271aaa5fa4fe729b7c922177c645e
suǝʞǝǝpʇ <tobias.deekens@epost-dev.de>
2015-10-26 16:55:17 +0100

    feat(topic-cloud): change styling using color-vars

--- grunt/tasks/postcss.js
+++ grunt/tasks/postcss.js
@@ -4,17 +4,9 @@ var
     _postcssImportProcessor = require('postcss-import')(),
     _postcssNestedProcessor = require('postcss-nested')(),
     _postcssNextProcessor = require('postcss-cssnext')(),
-    _postcssSvgProcessor = require('postcss-svg')({
-      ei: { "defaults": "[fill]: #333" }
-    }),
     _autoprefixerProcessor = require('autoprefixer')({
       browsers: [
-        'last 3 Android versions',
-        'last 4 iOS versions',
-        'last 5 Chrome versions',
-        'last 8 ChromeAndroid versions',
-        'last 4 ExplorerMobile versions',
-        'last 5 FirefoxAndroid versions'
+        '> 2%'
       ]
     }),
     _cssNanoProcessor = require('cssnano')();
@@ -28,7 +20,6 @@ module.exports = {
         _postcssImportProcessor,
         _postcssNestedProcessor,
         _postcssNextProcessor,
-        _postcssSvgProcessor,
         _postcssCustomMediaProcessor,
         _autoprefixerProcessor,
         _cssNanoProcessor
